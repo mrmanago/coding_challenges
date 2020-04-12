cc2c16eada4f4712eb7f7f39dde2925268c4a91c
mr manago <mrmanago@outlook.com>
2020-04-12 15:04:21 +0200

    WrongSubtraction

---
+++ WrongSubtraction/WrongSubtraction/Source.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+	for (int i; i = k; k--) {
+		if (n % 10 == 0) {
+			n /= 10;
+		} else {
+			n -= 1;
+		}
+	}
+	cout << n;
+
+	return 0;
+}
